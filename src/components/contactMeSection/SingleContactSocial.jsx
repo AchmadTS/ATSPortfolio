f985ecb062bd3572c7a2904a9eea404ef26e7075
Achmad Tirto Sudiro <achmadtirtosudirosudiro@gmail.com>
2025-04-07 18:39:39 +0700

    fix: SingleContactSocial

--- src/components/contactMeSection/SingleContactSocial.jsx
+++ src/components/contactMeSection/SingleContactSocial.jsx
@@ -1,11 +1,22 @@
+import PropTypes from "prop-types";
+
 const SingleContactSocial = ({ Icon, link }) => {
   return (
-    <div className="text-2xl h-12 w-12 border border-orange text-orange rounded-full p-3 flex items-center justify-center ">
-      <a href={link} className="cursor-pointer">
-        <Icon />
-      </a>
-    </div>
+    <a
+      href={link}
+      className="text-2xl h-12 w-12 border border-orange text-orange rounded-full p-3 flex items-center justify-center cursor-pointer
+                 hover:bg-orange hover:border-darkOrange hover:text-white transition-colors duration-500"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Icon />
+    </a>
   );
+};
+
+SingleContactSocial.propTypes = {
+  Icon: PropTypes.elementType.isRequired,
+  link: PropTypes.string.isRequired,
 };
 
 export default SingleContactSocial;
