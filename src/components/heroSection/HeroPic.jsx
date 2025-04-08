a1fbe2703783dc024a76719de7f459aace91c684
Achmad Tirto Sudiro <achmadtirtosudirosudiro@gmail.com>
2025-04-08 10:40:22 +0700

    refactor: image to webp

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="/images/Me.png"
+        src="/images/Me.webp"
         alt="Achmad Tirto Sudiro"
         className="max-h-[450px] w-auto"
       />
