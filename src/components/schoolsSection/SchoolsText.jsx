b607a2a19b3e29123d805ecab7698779fb23aec0
Achmad Tirto Sudiro <achmadtirtosudirosudiro@gmail.com>
2025-04-09 22:38:53 +0700

    chore

--- src/components/schoolsSection/SchoolsText.jsx
+++ src/components/schoolsSection/SchoolsText.jsx
@@ -1,7 +1,7 @@
 const SchoolsText = () => {
   return (
     <div className="flex flex-col items-center mt-[100px]">
-      <h2 className="text-6xl text-cyan mb-10">Education Histories</h2>
+      <h2 className="text-6xl text-cyan mb-10">Education</h2>
       <p className="text-lg text-center">
         The following is my educational background
       </p>
