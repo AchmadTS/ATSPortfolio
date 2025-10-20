25357d35173ef7ccb37e455111dee35322252e0d
AchmadTS <achmadtirtosudirosudiro@gmail.com>
2025-10-20 17:07:33 +0700

    update footer

--- src/components/footer/FooterMain.jsx
+++ src/components/footer/FooterMain.jsx
@@ -1,6 +1,12 @@
 import { Link } from "react-scroll";
 
 const FooterMain = () => {
+  const handleClick = () => {
+    const heroSection = document.getElementById("heroSection");
+    if (heroSection) {
+      heroSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   const footerLinks = [
     {
       name: "About Me",
@@ -18,12 +24,21 @@ const FooterMain = () => {
       name: "Projects",
       section: "projects",
     },
+    {
+      name: "Education",
+      section: "education",
+    },
   ];
   return (
     <div className="px-4">
       <div className="w-full h-[1px] bg-lightGrey mt-24"></div>
       <div className="md:flex justify-between mt-4 max-w-[1200px] mx-auto sm:hidden">
-        <p className="text-3xl text-lightGrey ">Achmad Tirto Sudiro</p>
+        <p
+          className="text-3xl text-lightGrey relative cursor-pointer hover:text-white transition-all duration-500"
+          onClick={handleClick}
+        >
+          Achmad Tirto Sudiro
+        </p>
         <ul className="flex gap-4 text-lightGrey text-xl">
           {footerLinks.map((item, index) => {
             return (
