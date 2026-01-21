8ab1eda5c41dee964b9275c3430635211131a2a2
AchmadTS <achmadtirtosudirosudiro@gmail.com>
2026-01-21 08:56:56 +0700

    feat: add mobile image variant for HeroImage and HeroPic components

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -9,16 +9,22 @@ const HeroPic = () => {
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0 }}
-      className=" h-full flex items-center justify-center "
+      className="relative h-full flex items-center justify-center"
     >
+      <img
+        src="/images/Me-Mobile.webp"
+        alt="Achmad Tirto Sudiro"
+        className="block md:hidden max-h-[240px] w-auto transition-transform duration-300"
+      />
+
       <img
         src="/images/Me.webp"
         alt="Achmad Tirto Sudiro"
-        className="max-h-[450px] w-auto transition-transform duration-300 hover:scale-105"
+        className="hidden md:block max-h-[450px] w-auto transition-transform duration-300 hover:scale-105"
       />
 
-      <div className=" absolute -z-10 flex justify-center items-center animate-pulse ">
-        <PiHexagonThin className="h-[475px] md:h-[600px] w-auto text-cyan blur-md" />
+      <div className="absolute -z-10 flex justify-center items-center animate-pulse">
+        <PiHexagonThin className="h-[320px] md:h-[600px] w-auto text-cyan blur-md" />
       </div>
     </motion.div>
   );
