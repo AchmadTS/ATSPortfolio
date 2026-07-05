517a8fbc632ffb91fbe6b33e9849274363c094ff
AchmadTS <achmadtirtosudirosudiro@gmail.com>
2026-07-05 07:46:17 +0700

    feat: add menu toggle functionality to NavbarLinks component

--- src/components/navbar/NavbarLinks.jsx
+++ src/components/navbar/NavbarLinks.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-scroll";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleMenu } from "../../state/menuSlice";
 
 const links = [
   { link: "About Me", section: "about" },
@@ -11,8 +13,16 @@ const links = [
 ];
 
 const NavbarLinks = () => {
+  const dispatch = useDispatch();
+  const menuOpen = useSelector((state) => state.menu.menuOpen);
+  const handleLinkClick = () => {
+    if (menuOpen) {
+      dispatch(toggleMenu());
+    }
+  };
+
   return (
-    <ul className="flex lg:flex-row sm:flex-col gap-6 text-white font-body lg:relative sm:absolute sm:top-[120%] text-center left-[50%] -translate-x-[50%] lg:text-md sm:text-xl sm:bg-cyan/30 backdrop-blur-lg lg:bg-black sm:w-full py-4">
+    <ul className="flex lg:flex-row sm:flex-col gap-6 text-white font-body lg:relative sm:absolute sm:top-[120%] text-center left-[50%] translate-x-[-50%] lg:text-md sm:text-xl sm:bg-cyan/30 backdrop-blur-lg lg:bg-black sm:w-full py-4">
       {links.map((link, index) => {
         return (
           <li key={index} className="group">
@@ -24,10 +34,11 @@ const NavbarLinks = () => {
               to={link.section}
               activeClass="active"
               className="cursor-pointer text-white hover:text-cyan transition-all duration-500"
+              onClick={handleLinkClick}
             >
               {link.link}
             </Link>
-            <div className="mx-auto bg-cyan w-0 group-hover:w-full h-[1px] transition-all duration-500"></div>
+            <div className="mx-auto bg-cyan w-0 group-hover:w-full h-px transition-all duration-500"></div>
           </li>
         );
       })}
