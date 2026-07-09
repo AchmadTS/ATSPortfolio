2278968de92eea7ca6ffec3c683e48ac82caa024
AchmadTS <achmadtirtosudirosudiro@gmail.com>
2026-07-09 16:29:39 +0700

    refactor: simplify typing effect logic in TypewriterMessage component

--- src/components/chatPopup/TypewriterMessage.jsx
+++ src/components/chatPopup/TypewriterMessage.jsx
@@ -5,22 +5,28 @@ import { markdownComponents } from "./MarkdownComponents";
 // eslint-disable-next-line react/prop-types
 const TypewriterMessage = ({ content, onDone, alreadyTyped }) => {
   const [displayed, setDisplayed] = useState(alreadyTyped ? content : "");
-  // eslint-disable-next-line react/prop-types
-  const [index, setIndex] = useState(alreadyTyped ? content.length : 0);
+
   useEffect(() => {
     if (alreadyTyped) return;
 
-    // eslint-disable-next-line react/prop-types
-    if (index < content.length) {
-      const timeout = setTimeout(() => {
-        setDisplayed((prev) => prev + content[index]);
-        setIndex((prev) => prev + 1);
-      }, 20);
-      return () => clearTimeout(timeout);
-    } else {
-      onDone();
-    }
-  }, [index, content, alreadyTyped, onDone]);
+    const typingSpeed = 20;
+    const startTime = Date.now();
+    const fullText = content;
+    const interval = setInterval(() => {
+      const elapsedTime = Date.now() - startTime;
+      const expectedCharCount = Math.floor(elapsedTime / typingSpeed);
+
+      if (expectedCharCount >= fullText.length) {
+        setDisplayed(fullText);
+        clearInterval(interval);
+        onDone();
+      } else {
+        setDisplayed(fullText.substring(0, expectedCharCount));
+      }
+    }, 20);
+
+    return () => clearInterval(interval);
+  }, [content, alreadyTyped, onDone]);
 
   return (
     <ReactMarkdown components={markdownComponents}>{displayed}</ReactMarkdown>
