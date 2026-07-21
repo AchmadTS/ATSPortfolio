44f3b190863eb3182fe3e67030aa973b4c52fc24
AchmadTS <achmadtirtosudirosudiro@gmail.com>
2026-07-21 08:47:31 +0700

    feat: enhance SingleSkill component with aria-label for improved accessibility

--- src/components/skillsSection/SingleSkill.jsx
+++ src/components/skillsSection/SingleSkill.jsx
@@ -1,11 +1,14 @@
 import PropTypes from "prop-types";
+import React from "react";
 
 const SingleSkill = ({ imgSvg, text }) => {
   return (
     <div className="hover:-translate-y-10 transition-all duration-500 group">
       <div className="flex flex-col items-center gap-2 relative">
         <div className="bg-white text-cyan h-25 w-25 flex items-center justify-center rounded-full hover:text-darkGrey hover:scale-105 transform transition-all duration-500 text-6xl border-4 border-orange">
-          {imgSvg}
+          {React.cloneElement(imgSvg, {
+            "aria-label": `Logo ${text}`,
+          })}
         </div>
         <p className="text-white font-bold">{text}</p>
         {/* <p className="text-white font-bold opacity-0 group-hover:opacity-100 transition-opacity duration-500">
