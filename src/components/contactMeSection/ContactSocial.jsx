6773941663f6ace5f7716eef91738dba4e294d26
AchmadTS <achmadtirtosudirosudiro@gmail.com>
2026-07-21 08:30:31 +0700

    feat: add aria-label attributes to social media icons for improved accessibility

--- src/components/contactMeSection/ContactSocial.jsx
+++ src/components/contactMeSection/ContactSocial.jsx
@@ -8,15 +8,15 @@ const ContactSocial = () => {
     <div className="flex gap-4">
       <SingleContactSocial
         link="https://id.linkedin.com/in/achmad-tirto-sudiro-368aa6304"
-        Icon={FaLinkedinIn}
+        Icon={FaLinkedinIn} aria-label="Kunjungi Profil LinkedIn"
       />
       <SingleContactSocial
         link="https://github.com/Achmadts/"
-        Icon={FiGithub}
+        Icon={FiGithub} aria-label="Kunjungi Profil GitHub"
       />
       <SingleContactSocial
         link="https://www.instagram.com/ats090906/"
-        Icon={FaInstagram}
+        Icon={FaInstagram} aria-label="Kunjungi Profil Instagram"
       />
     </div>
   );
