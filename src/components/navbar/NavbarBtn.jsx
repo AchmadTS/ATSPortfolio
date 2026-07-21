a86af17039c6ecf2908334afd6005d22a5b3c468
AchmadTS <achmadtirtosudirosudiro@gmail.com>
2026-07-21 08:43:07 +0700

    feat: refactor NavbarBtn to use Link component for navigation and improve accessibility

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -3,21 +3,20 @@ import { LuArrowDownRight } from "react-icons/lu";
 
 const NavbarBtn = () => {
   return (
-    <button className="px-4 py-2 rounded-full font-bold font-body text-white border-cyan border flex items-center gap-1 bg-linear-to-r from-darkCyan to-orange transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow">
-      <Link
-        spy={false}
-        smooth={true}
-        duration={500}
-        offset={-120}
-        to="contact"
-        className="text-base sm:text-lg md:text-xl"
-      >
-        Hire Me
-      </Link>
+    <Link
+      spy={false}
+      smooth={true}
+      duration={500}
+      offset={-120}
+      to="contact"
+      href="#contact"
+      className="px-4 py-2 rounded-full font-bold font-body text-white border-cyan border flex items-center gap-1 bg-linear-to-r from-darkCyan to-orange transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow"
+    >
+      <span className="text-base sm:text-lg md:text-xl">Hire Me</span>
       <div className="sm:hidden md:block">
         <LuArrowDownRight />
       </div>
-    </button>
+    </Link>
   );
 };
 
